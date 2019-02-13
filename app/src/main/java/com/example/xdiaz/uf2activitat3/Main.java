8b7154f7da526aed2544b9b00887571a36be5964
Ernestodiazxd <xdiaz17@jaumebalmes.net>
2019-02-13 20:51:11 +0100

    v0.1

---
+++ app/src/main/java/com/example/xdiaz/uf2activitat3/Main.java
@@ -0,0 +1,50 @@
+package com.example.xdiaz.uf2activitat3;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        }
+
+
+
+    }
+
+    public void onClickAuxili(View v){
+
+        Intent intent = new Intent(Main.this, Auxili.class);
+        startActivity(intent);
+
+
+    }
+
+    public void onClickLocalitzador(View v){
+
+        Intent intent = new Intent(Main.this, Localitzador.class);
+        startActivity(intent);
+
+
+    }
+
+
+}
